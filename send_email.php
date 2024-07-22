a76f0e8d6660dedbea633db1fed08f37a5addf79
resodik <153916452+resodik@users.noreply.github.com>
2024-07-22 14:29:26 +0200

    Update and rename przeniesione/new.html to send_email.php

---
+++ send_email.php
@@ -0,0 +1,30 @@
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Zbierz dane z formularza
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Ustawienia e-maila
+    $to = 'r.sobieniak.grafika@gmail.com';
+    $subject = 'Nowa wiadomość kontaktowa';
+    
+    // Treść e-maila
+    $body = "Imię: $name\n";
+    $body .= "E-mail: $email\n";
+    $body .= "Wiadomość:\n$message\n";
+    
+    // Nagłówki e-maila
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    
+    // Wyślij e-mail
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Wiadomość została wysłana!";
+    } else {
+        echo "Wystąpił problem podczas wysyłania wiadomości.";
+    }
+}
+?>
