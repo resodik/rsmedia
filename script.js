9814d35f95e396477c250b1cdd11b43dc0d59c11
resodik <153916452+resodik@users.noreply.github.com>
2025-06-24 02:13:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -261,3 +261,12 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+<script>
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('navLinks');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.style.display =
+      navLinks.style.display === 'flex' ? 'none' : 'flex';
+  });
+</script>
